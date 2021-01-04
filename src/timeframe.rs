efcda84a9b377b9dcfb359f57c0f62a727582fa3
Andrey Kuznetsov <fear@loathing.in>
2021-01-04 19:27:23 +0000

    Bar struct with close value and stop dt

--- src/timeframe.rs
+++ src/timeframe.rs
@@ -1,18 +1,24 @@
 use chrono::prelude::*;
 
 #[derive(Debug, PartialEq)]
-pub enum Bar {
+pub struct Bar {
+    close: f64,
+    stop_dt: NaiveDateTime,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Bars {
     // closing value
-    Single(f64),
+    Single(Bar),
     // closing value and count of empty bars
-    WithEmpty(f64, usize),
+    WithEmpty(Bar, Vec<Bar>),
 }
 
 pub trait Sampler {
     /// Returns Some(price) if period has been passed, None otherwise
-    fn next(&mut self, dt: NaiveDateTime, value: f64) -> Option<Bar>;
+    fn next_bar(&mut self, dt: NaiveDateTime, value: f64) -> Option<Bars>;
 
-    fn next_bar(&self, dt: NaiveDateTime) -> chrono::NaiveDateTime;
+    fn next_bar_dt(&self, dt: NaiveDateTime) -> chrono::NaiveDateTime;
 }
 
 macro_rules! sampler {
@@ -32,14 +38,14 @@ macro_rules! sampler {
 
 #[derive(Debug)]
 struct State {
-    next_bar: NaiveDateTime,
+    next_bar_dt: NaiveDateTime,
     last_value: f64,
 }
 
 impl State {
-    fn new(next_bar: NaiveDateTime, last_value: f64) -> Self {
+    fn new(next_bar_dt: NaiveDateTime, last_value: f64) -> Self {
         Self {
-            next_bar,
+            next_bar_dt,
             last_value,
         }
     }
@@ -47,39 +53,46 @@ impl State {
 
 macro_rules! next {
     () => {
-        fn next(&mut self, dt: NaiveDateTime, value: f64) -> Option<Bar> {
+        fn next_bar(&mut self, dt: NaiveDateTime, value: f64) -> Option<Bars> {
             match self.state {
                 Some(State {
-                    next_bar,
+                    next_bar_dt,
                     last_value,
                 }) => {
-                    if dt >= next_bar {
-                        let mut empty_count = 0;
-                        // woohoo!
+                    if dt >= next_bar_dt {
+                        let full_bar = Bar {
+                            close: last_value,
+                            stop_dt: next_bar_dt,
+                        };
+
                         // TODO: TwoHardThings
-                        let mut next_bar = self.next_bar(next_bar);
-                        while dt >= next_bar {
-                            empty_count += 1;
-                            next_bar = self.next_bar(next_bar);
+                        // woohoo!
+                        let mut next_bar_dt = self.next_bar_dt(next_bar_dt);
+
+                        let mut empty_bars = vec![];
+                        while dt >= next_bar_dt {
+                            empty_bars.push(Bar {
+                                close: last_value,
+                                stop_dt: next_bar_dt,
+                            });
+                            next_bar_dt = self.next_bar_dt(next_bar_dt);
                         }
 
-                        self.state = Some(State::new(next_bar, value));
+                        self.state = Some(State::new(next_bar_dt, value));
 
-                        if empty_count > 0 {
-                            Some(Bar::WithEmpty(last_value, empty_count as usize))
-                        } else if empty_count < 0 {
-                            panic!("{} is less than previous tick date", dt);
+                        if empty_bars.len() > 0 {
+                            Some(Bars::WithEmpty(full_bar, empty_bars))
                         } else {
-                            Some(Bar::Single(last_value))
+                            Some(Bars::Single(full_bar))
                         }
                     } else {
-                        self.state = Some(State::new(next_bar, value));
+                        self.state = Some(State::new(next_bar_dt, value));
                         None
                     }
                 }
                 None => {
-                    let next_bar = self.next_bar(dt);
-                    self.state = Some(State::new(next_bar, value));
+                    let next_bar_dt = self.next_bar_dt(dt);
+                    self.state = Some(State::new(next_bar_dt, value));
                     None
                 }
             }
@@ -95,7 +108,7 @@ macro_rules! Minute {
             next!();
 
             #[allow(clippy::modulo_one)]
-            fn next_bar(&self, dt: NaiveDateTime) -> NaiveDateTime {
+            fn next_bar_dt(&self, dt: NaiveDateTime) -> NaiveDateTime {
                 dt.date()
                     .and_hms(dt.hour(), 0, 0)
                     .checked_add_signed(chrono::Duration::minutes(
@@ -115,7 +128,7 @@ macro_rules! Hour {
             next!();
 
             #[allow(clippy::modulo_one)]
-            fn next_bar(&self, dt: NaiveDateTime) -> NaiveDateTime {
+            fn next_bar_dt(&self, dt: NaiveDateTime) -> NaiveDateTime {
                 dt.date()
                     .and_hms(0, 0, 0)
                     .checked_add_signed(chrono::Duration::hours(
@@ -151,7 +164,7 @@ sampler!(D1);
 impl Sampler for D1 {
     next!();
 
-    fn next_bar(&self, dt: NaiveDateTime) -> NaiveDateTime {
+    fn next_bar_dt(&self, dt: NaiveDateTime) -> NaiveDateTime {
         dt.date()
             .and_hms(0, 0, 0)
             .checked_add_signed(chrono::Duration::days(1))
@@ -163,12 +176,13 @@ sampler!(W1);
 impl Sampler for W1 {
     next!();
 
-    fn next_bar(&self, dt: NaiveDateTime) -> chrono::NaiveDateTime {
+    fn next_bar_dt(&self, dt: NaiveDateTime) -> chrono::NaiveDateTime {
         let weekday = dt.weekday();
+        let sub = weekday.num_days_from_monday() as i64;
+        let add = 7 - sub;
+        // println!("weekday is {}, sub is {}, add is {}", weekday, sub, add);
         dt.date()
-            .checked_add_signed(chrono::Duration::days(
-                7 - weekday.num_days_from_monday() as i64,
-            ))
+            .checked_add_signed(chrono::Duration::days(add))
             .unwrap()
             .and_hms(0, 0, 0)
     }
@@ -178,7 +192,7 @@ sampler!(MN1);
 impl Sampler for MN1 {
     next!();
 
-    fn next_bar(&self, dt: NaiveDateTime) -> chrono::NaiveDateTime {
+    fn next_bar_dt(&self, dt: NaiveDateTime) -> chrono::NaiveDateTime {
         let date = dt.date();
         let date = if date.month() == 12 {
             // FIXME: bug with B.C.?
@@ -223,98 +237,244 @@ mod test {
     #[test]
     fn test_m15() {
         let mut sampler = M15::default();
-        let res = sampler.next(date("2015-01-01 10:03:00"), 0.);
+        let res = sampler.next_bar(date("2015-01-01 10:03:00"), 0.);
         assert_eq!(res, None);
-        let res = sampler.next(date("2015-01-01 10:04:00"), 4.);
+        let res = sampler.next_bar(date("2015-01-01 10:04:00"), 4.);
         assert_eq!(res, None);
 
         // new period start, should return prev period closing value
-        let res = sampler.next(date("2015-01-01 10:15:00"), 15.);
-        assert_eq!(res, Some(Bar::Single(4.)));
+        let res = sampler.next_bar(date("2015-01-01 10:15:00"), 15.);
+        assert_eq!(
+            res,
+            Some(Bars::Single(Bar {
+                close: 4.,
+                stop_dt: date("2015-01-01 10:15:00")
+            }))
+        );
 
         // 15-30 period hasn't passed, should return last period close value
-        let res = sampler.next(date("2015-01-01 10:15:01"), 15.);
+        let res = sampler.next_bar(date("2015-01-01 10:15:01"), 15.);
         assert_eq!(res, None);
-        let res = sampler.next(date("2015-01-01 10:15:02"), 16.);
+        let res = sampler.next_bar(date("2015-01-01 10:15:02"), 16.);
         assert_eq!(res, None);
 
         // new period
-        let res = sampler.next(date("2015-01-01 10:45:02"), 45.);
-        assert_eq!(res, Some(Bar::WithEmpty(16., 1)));
+        let res = sampler.next_bar(date("2015-01-01 10:45:02"), 45.);
+        assert_eq!(
+            res,
+            Some(Bars::WithEmpty(
+                Bar {
+                    close: 16.,
+                    stop_dt: date("2015-01-01 10:30:00")
+                },
+                vec![Bar {
+                    close: 16.,
+                    stop_dt: date("2015-01-01 10:45:00")
+                }]
+            ))
+        );
     }
 
     #[test]
     fn test_h12() {
         let mut sampler = H12::default();
-        let res = sampler.next(date("2015-01-01 01:03:00"), 0.);
+        let res = sampler.next_bar(date("2015-01-01 01:03:00"), 0.);
         assert_eq!(res, None);
-        let res = sampler.next(date("2015-01-01 01:04:00"), 4.);
+        let res = sampler.next_bar(date("2015-01-01 01:04:00"), 4.);
         assert_eq!(res, None);
 
         // new period start, should return prev period closing value
-        let res = sampler.next(date("2015-01-01 12:00:00"), 15.);
-        assert_eq!(res, Some(Bar::Single(4.)));
+        let res = sampler.next_bar(date("2015-01-01 12:00:00"), 15.);
+        assert_eq!(
+            res,
+            Some(Bars::Single(Bar {
+                close: 4.,
+                stop_dt: date("2015-01-01 12:00:00")
+            }))
+        );
 
         // 12-24 period hasn't passed, should return last period close value
-        let res = sampler.next(date("2015-01-01 13:00:00"), 15.);
+        let res = sampler.next_bar(date("2015-01-01 13:00:00"), 15.);
         assert_eq!(res, None);
 
         // new period
-        let res = sampler.next(date("2015-01-03 10:45:02"), 45.);
-        assert_eq!(res, Some(Bar::WithEmpty(15., 2)));
+        let res = sampler.next_bar(date("2015-01-03 10:45:02"), 45.);
+        assert_eq!(
+            res,
+            Some(Bars::WithEmpty(
+                Bar {
+                    close: 15.,
+                    stop_dt: date("2015-01-02 00:00:00")
+                },
+                vec![
+                    Bar {
+                        close: 15.,
+                        stop_dt: date("2015-01-02 12:00:00")
+                    },
+                    Bar {
+                        close: 15.,
+                        stop_dt: date("2015-01-03 00:00:00")
+                    },
+                ]
+            ))
+        );
     }
 
     #[test]
     fn test_d1() {
         let mut sampler = D1::default();
-        let res = sampler.next(date("2015-01-03 10:45:02"), 0.);
+        let res = sampler.next_bar(date("2015-01-03 10:45:02"), 0.);
         assert_eq!(res, None);
 
-        let res = sampler.next(date("2015-01-04 00:00:00"), 1.);
-        assert_eq!(res, Some(Bar::Single(0.)));
+        let res = sampler.next_bar(date("2015-01-04 00:00:00"), 1.);
+        assert_eq!(
+            res,
+            Some(Bars::Single(Bar {
+                close: 0.,
+                stop_dt: date("2015-01-04 00:00:00")
+            }))
+        );
 
-        let res = sampler.next(date("2015-01-07 00:00:00"), 2.);
+        let res = sampler.next_bar(date("2015-01-07 00:00:00"), 2.);
         // 05 and 06 are empty
-        assert_eq!(res, Some(Bar::WithEmpty(1., 2)))
+        assert_eq!(
+            res,
+            Some(Bars::WithEmpty(
+                Bar {
+                    close: 1.,
+                    stop_dt: date("2015-01-05 00:00:00")
+                },
+                vec![
+                    Bar {
+                        close: 1.,
+                        stop_dt: date("2015-01-06 00:00:00")
+                    },
+                    Bar {
+                        close: 1.,
+                        stop_dt: date("2015-01-07 00:00:00")
+                    },
+                ]
+            ))
+        )
     }
 
     #[test]
     fn test_w1() {
         let mut sampler = W1::default();
         // monday
-        let res = sampler.next(date("2020-01-04 10:45:02"), 0.);
+        let res = sampler.next_bar(date("2021-01-04 10:45:02"), 0.);
         assert_eq!(res, None);
 
         // tuesday
-        let res = sampler.next(date("2020-01-05 00:00:00"), 1.);
+        let res = sampler.next_bar(date("2021-01-05 00:00:00"), 1.);
         assert_eq!(res, None);
 
-        // The next monday
-        let res = sampler.next(date("2020-01-11 00:00:00"), 2.);
-        assert_eq!(res, Some(Bar::Single(1.)));
+        // The next_bar monday
+        let res = sampler.next_bar(date("2021-01-11 00:00:00"), 2.);
+        assert_eq!(
+            res,
+            Some(Bars::Single(Bar {
+                close: 1.,
+                stop_dt: date("2021-01-11 00:00:00")
+            }))
+        );
 
         // Two weeks later, tuesday
-        let res = sampler.next(date("2020-01-26 00:00:00"), 3.);
-        assert_eq!(res, Some(Bar::WithEmpty(2., 1)));
+        let res = sampler.next_bar(date("2021-01-26 00:00:00"), 3.);
+        assert_eq!(
+            res,
+            Some(Bars::WithEmpty(
+                Bar {
+                    close: 2.,
+                    stop_dt: date("2021-01-18 00:00:00")
+                },
+                vec![Bar {
+                    close: 2.,
+                    stop_dt: date("2021-01-25 00:00:00")
+                }]
+            ))
+        );
     }
 
     #[test]
     fn test_mn1() {
         let mut sampler = MN1::default();
-        let res = sampler.next(date("2020-01-01 10:45:02"), 0.);
+        let res = sampler.next_bar(date("2020-01-01 10:45:02"), 0.);
         assert_eq!(res, None);
 
-        let res = sampler.next(date("2020-01-02 00:00:00"), 1.);
+        let res = sampler.next_bar(date("2020-01-02 00:00:00"), 1.);
         assert_eq!(res, None);
 
-        let res = sampler.next(date("2020-02-02 00:00:00"), 2.);
-        assert_eq!(res, Some(Bar::Single(1.)));
+        let res = sampler.next_bar(date("2020-02-02 00:00:00"), 2.);
+        assert_eq!(
+            res,
+            Some(Bars::Single(Bar {
+                close: 1.,
+                stop_dt: date("2020-02-01 00:00:00")
+            }))
+        );
 
-        let res = sampler.next(date("2020-10-26 00:00:00"), 3.);
-        assert_eq!(res, Some(Bar::WithEmpty(2., 7)));
+        let res = sampler.next_bar(date("2020-10-26 00:00:00"), 3.);
+        assert_eq!(
+            res,
+            Some(Bars::WithEmpty(
+                Bar {
+                    close: 2.,
+                    stop_dt: date("2020-03-01 00:00:00")
+                },
+                vec![
+                    Bar {
+                        close: 2.,
+                        stop_dt: date("2020-04-01 00:00:00")
+                    },
+                    Bar {
+                        close: 2.,
+                        stop_dt: date("2020-05-01 00:00:00")
+                    },
+                    Bar {
+                        close: 2.,
+                        stop_dt: date("2020-06-01 00:00:00")
+                    },
+                    Bar {
+                        close: 2.,
+                        stop_dt: date("2020-07-01 00:00:00")
+                    },
+                    Bar {
+                        close: 2.,
+                        stop_dt: date("2020-08-01 00:00:00")
+                    },
+                    Bar {
+                        close: 2.,
+                        stop_dt: date("2020-09-01 00:00:00")
+                    },
+                    Bar {
+                        close: 2.,
+                        stop_dt: date("2020-10-01 00:00:00")
+                    },
+                ]
+            ))
+        );
 
-        let res = sampler.next(date("2021-01-01 00:00:01"), 3.);
-        assert_eq!(res, Some(Bar::WithEmpty(3., 2)));
+        let res = sampler.next_bar(date("2021-01-01 00:00:01"), 3.);
+        assert_eq!(
+            res,
+            Some(Bars::WithEmpty(
+                Bar {
+                    close: 3.,
+                    stop_dt: date("2020-11-01 00:00:00")
+                },
+                vec![
+                    Bar {
+                        close: 3.,
+                        stop_dt: date("2020-12-01 00:00:00")
+                    },
+                    Bar {
+                        close: 3.,
+                        stop_dt: date("2021-01-01 00:00:00")
+                    },
+                ]
+            ))
+        );
     }
 
     fn date(date_str: &str) -> NaiveDateTime {
